5aa570f4665f232cb46b632270ac4bc9a7242ad2
sachsmc <sachsmc@gmail.com>
2013-11-12 13:50:09 -0500

    Description Update

--- R/predcurver-package.r
+++ R/predcurver-package.r
@@ -1,4 +1,6 @@
-#' predcurver
+#' Class and methods for the predictiveness curve
+#' 
+#' Estimates the predictiveness curve from a vector of risks (true or estimated). Plots, prints, summarizes, and tests using the predcurve S3 class.
 #'
 #' @name predcurver
 #' @docType package
